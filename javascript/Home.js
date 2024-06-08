bdf5fba3eaac58905b73eb9d8699202ee9395183
gyojunChae <122966780+gyojunChae@users.noreply.github.com>
2024-06-08 23:20:23 +0900

    로그인창
    모양은 성공 기능은 실패

---
+++ javascript/Home.js
@@ -0,0 +1,7 @@
+// Navbar JavaScript
+function toggleMenu() {
+  const navMenu = document.querySelector(".nav_menu");
+  const navIcon = document.querySelector(".nav_icon");
+  navMenu.classList.toggle("active");
+  navIcon.classList.toggle("active");
+}
