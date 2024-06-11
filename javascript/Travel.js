05611a187a634d8da6d4465faffe3c4087d9838e
gyojunChae <122966780+gyojunChae@users.noreply.github.com>
2024-06-11 15:06:37 +0900

    중간업데이트
    만드는중

--- javascript/Travel.js
+++ javascript/Travel.js
@@ -4,3 +4,14 @@ function toggleMenu() {
   navMenu.classList.toggle("active");
   navIcon.classList.toggle("active");
 }
+
+function searchGoogle() {
+  // 검색어 입력란에서 입력한 값을 가져옵니다.
+  var query = document.getElementById("search-input").value;
+
+  // Google 검색 URL을 생성합니다.
+  var googleSearchURL = "https://www.google.com/search?q=" + encodeURIComponent(query);
+
+  // 새 창에서 Google 검색을 엽니다.
+  window.open(googleSearchURL, "_blank");
+}
